a375daaf02805f4dedeafe625a6d41882c0e8778
alex <alex.zhang@meraki.net>
2020-07-28 22:10:35 -0700

    add styling to home page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,10 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +13,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("jquery")
+import 'bootstrap'  //https://blog.makersacademy.com/how-to-install-bootstrap-and-jquery-on-rails-6-da6e810c1b87
+import 'select2'
+import 'select2/dist/css/select2.css'
+import 'tilt.js'
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default'],
+    tilt: 'tilt.js/src/tilt.jquery',
+  })
+)
+
 module.exports = environment
